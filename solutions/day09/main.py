108a62646f4a9d73d9f8b7338ff29bfef0e0d1e9
Mannala <manfred.stenzel@gmail.com>
2026-08-29 21:47:10 +0200

    Day 9 renewed >> not finished

--- solutions/day09/main.py
+++ solutions/day09/main.py
@@ -3,25 +3,44 @@ import os
 
 print(logo)
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_name():
+    name = input("Please enter your name: ").strip()
+    if not name:
+        print("You didn't enter anything. Please try again.\n")
+        return get_name()
+    if name in bidders_dictionary:
+        print("The name already exists. Please enter a different Name.")
+        return get_name
+    return name
+
+def get_bid():
+    bid_string = input("What is your bid: ")
+    if not bid_string:
+        print("You didn't enter anything. Please try again.\n")
+        return get_bid()
+    try:
+        bid = int(bid_string)
+    except ValueError:
+        print("Invalid input. Please enter an integer.\n")
+        return get_bid()
+    return bid
+
 bidders_dictionary = {}
 
-def clear_screen():
-    os.system("cls" if os.name == "nt" else "clear")
-
-game_on = True
-while game_on:
-    name = input("What is you name?: ")
-    while name in bidders_dictionary:
-        name += '_2'
-    bid = int(input("What is your bid?: $"))
+game_mode = True
+while game_mode:
+    name = get_name()
+    bid = get_bid()
     bidders_dictionary[name] = bid
-    more_bidders = input("Are there any other bidders? Type 'yes' or no'.\n")
-    if more_bidders == "yes":
-        clear_screen()
-    elif more_bidders == "no":
-        for key in bidders_dictionary:
-            winner = key
-            if bidders_dictionary[key] > bidders_dictionary[winner]:
-                winner = key
-        print(f"The winner is {winner} with a bid of ${bidders_dictionary[winner]}")
-        game_on = False
+    while True:
+        choice = input("Are there any other bidders? Type 'yes' or 'no'.").strip().lower()
+        if choice in ['yes', 'no']:
+            break
+        else:
+            print("Wrong answer. Please type in 'yes' or 'no'")
+    if choice == 'yes':
+        
+    
