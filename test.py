691f2d5fdd4a6ff938009d2b4e91de66b7f4c491
Mannala <manfred.stenzel@gmail.com>
2026-09-18 20:59:11 +0200

    I dunno

--- test.py
+++ test.py
@@ -1,28 +1,9 @@
-student_scores = {
-    'Harry': 88,
-    'Ron': 78,
-    'Hermione': 95,
-    'Draco': 75,
-    'Neville': 160
-}
-
-
-def student_value_to_grading(value):
-    if value > 100 or value < 0:
-        raise ValueError("Value must be positive")
-    elif value >= 91:
-        return "Outstanding"
-    elif value >= 81:
-        return "Exceeds Expectations"
-    elif value >= 71:
-        return "Acceptable"
+def my_function(a):
+    if a < 40:
+        return
+        print("Terrible")
+    if a < 80:
+        return "Pass"
     else:
-        return "Fail"
-
-
-student_grades = {}
-for name in student_scores:
-    student_grades[name] = student_value_to_grading(student_scores[name])
-    
-print(student_grades)
-    
+        return "Great"
+print(my_function(25))
